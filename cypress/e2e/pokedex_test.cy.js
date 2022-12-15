d434ce1a94902e8148a1f63d9922708fa2ca0f5b
Steffengj <103410399+SteffenGJ@users.noreply.github.com>
2022-12-15 13:21:51 +0100

    same as before

--- cypress/e2e/pokedex_test.cy.js
+++ cypress/e2e/pokedex_test.cy.js
@@ -6,7 +6,8 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('works when you navigate to a specific pokemon', function() {
-    cy.visit('http://localhost:5000/pokemon/ivysaur')
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
   })
 })
